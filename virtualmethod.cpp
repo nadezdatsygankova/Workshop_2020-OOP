087e4d567864dbf758e2a7391974dc889fd9f7ad
Nadezda Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2020-10-27 23:37:50 -0400

    Add files via upload
    #103

---
+++ virtualmethod.cpp
@@ -0,0 +1,55 @@
+﻿
+
+#include <iostream>
+
+class Gun
+{
+public:
+   virtual void Shoot()
+   {
+      std::cout << "BUM" << std::endl;
+   }
+};
+
+class SubmachineGin : public Gun
+{
+public:
+   void Shoot() override
+   {
+      std::cout << "BUM BUM BUM" << std::endl;
+   }
+};
+
+class Bazzoka :public Gun
+{
+public:
+   void Shoot() override
+   {
+      std::cout << "BABAM" << std::endl;
+   }
+};
+
+class Human
+{
+public:
+   void Use(Gun* gun)
+   {
+      gun->Shoot();
+   }
+};
+int main()
+{
+   Gun g;
+   g.Shoot();
+
+   SubmachineGin sub;
+   sub.Shoot();
+
+   Bazzoka baz;
+   baz.Shoot();
+
+   Human first;
+   first.Use(&baz);
+
+
+}
