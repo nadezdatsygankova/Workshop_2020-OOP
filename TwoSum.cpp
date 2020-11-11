6eb2914510b567e78c527763be157304eab3a424
Nadezda Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2020-11-11 13:22:50 -0500

    Add files via upload

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,39 +1,8 @@
-/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
- 
-
-Example 1:
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
-
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
- 
-
-Constraints:
-
-2 <= nums.length <= 105
--109 <= nums[i] <= 109
--109 <= target <= 109
-Only one valid answer exists.
-*/
-
-
 #include <algorithm>
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include<cmath>
 
 
 class Solution {
@@ -41,23 +10,27 @@ public:
    std::vector<int> twoSum(std::vector<int>& nums, int target) {
       std::vector <int> res;
       int i;
-     
-      for ( i=0;i< nums.size()-1;i++)
+      if (nums.size() >= 2 && nums.size() <= pow(10, 5) && target >= pow(-10, 9) && target <= pow(10, 9))
       {
-         for (int j = i+1; j<nums.size(); j++)
+         for (i = 0; i < nums.size() - 1; i++)
          {
-            if ((nums[i] + nums[j]) == target)
+            if (nums[i] >= pow(-10, 9) && nums[i] <= pow(10, 9))
             {
-               res.push_back(i);
-               res.push_back(j);
-               return res;
+               for (int j = i + 1; j < nums.size(); j++)
+               {
+                  if ((nums[i] + nums[j]) == target)
+                  {
+                     res.push_back(i);
+                     res.push_back(j);
+                     return res;
+                  }
+               }
             }
-
          }
-        
-      }
+     }
       return res;
    }
+  
 };
 
 
